e025197e81f16222b6764788885eadb79afeb729
kamekazz <taveras78@gmail.com>
2020-05-28 20:33:37 -0400

    statar questions

--- components/screens/IncomingSample/AddModel/index.js
+++ components/screens/IncomingSample/AddModel/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
 
 import { connect } from "react-redux";
 import { ContainerEl, TextEl } from "../../../../style/element";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styled from "styled-components/native";
+import { COLORS } from "../../../../constants/Colors";
+import { View, StyleSheet } from "react-native";
+
+data = {
+  questions: ["", "", "", "", "", "", "", "", "", ""],
+};
 
 export class index extends Component {
   render() {
     return (
-      <ContainerEl style={{ backgroundColor: "red" }}>
-        <TextEl> AddModel </TextEl>
-        <Button title="close" onPress={() => this.props.modelOption(false)} />
-      </ContainerEl>
+      <Container>
+        <View style={StyleSheet.absoluteFill}></View>
+        <OptionBottom onPress={this.handleAnswer} activeOpacity={0.7}>
+          <AnswerText>NO</AnswerText>
+        </OptionBottom>
+        <OptionBottom onPress={this.handleAnswer} activeOpacity={0.7}>
+          <AnswerText>YES</AnswerText>
+        </OptionBottom>
+      </Container>
     );
   }
 }
@@ -20,3 +32,19 @@ const mapStateToProps = (state) => ({});
 const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(index);
+
+const Container = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const OptionBottom = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const AnswerText = styled(TextEl)`
+  font-size: 30px;
+  margin-bottom: 50px;
+`;
